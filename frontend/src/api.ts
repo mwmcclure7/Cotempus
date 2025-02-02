aebaea52843e1742c1bb3dc243431d89c5320621
mwmcclure7 <106849204+mwmcclure7@users.noreply.github.com>
2025-02-02 00:03:28 -0500

    Added serviceurl

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : "/choreo-apis/backend-2184680224:8000",
 });
 
 api.interceptors.request.use(
