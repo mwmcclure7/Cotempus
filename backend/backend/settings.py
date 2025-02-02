e6147a5eec7af0191f302d90c9d091cc67f7714f
mwmcclure7 <106849204+mwmcclure7@users.noreply.github.com>
2025-02-02 01:10:26 -0500

    Added link to allowed_hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv("DJANGO_ALLOWED_HOSTS")]
 
 # Application definition
 
