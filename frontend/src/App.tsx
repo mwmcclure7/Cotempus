b21823aa8db1d4dfb7a1f710c681976a6c41aa18
mwmcclure7 <106849204+mwmcclure7@users.noreply.github.com>
2025-02-02 22:51:50 -0500

    Use hashrouter instead

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import CreateSchedule from './pages/CreateSchedule'
 import JoinSchedule from './pages/JoinSchedule'
