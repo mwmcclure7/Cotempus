e29068a1074228ea10122953e5587ad34167e58f
mwmcclure7 <106849204+mwmcclure7@users.noreply.github.com>
2025-02-02 22:26:24 -0500

    Eliminated choreo link

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,11 +1,7 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
-        ? import.meta.env.VITE_API_URL
-        : "/choreo-apis/cotempus/backend/v1",
-});
+const api = axios.create({baseURL: import.meta.env.VITE_API_URL});
 
 api.interceptors.request.use(
     (config) => {
